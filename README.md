0a2b758884a7be6703f4869cdeb5a8bd8fc090ec
Julien Defferrard <julien.defferrard@morphean.com>
2024-08-16 09:26:49 +0200

    integrate docker

--- README.md
+++ README.md
@@ -9,11 +9,27 @@ The purpose is testing the AWS RDS connection using the AWS advanced JDBC wrappe
 Create a new file named `application.properties` in the `src/main/resources` folder and add the following content:
 
 ```properties
-url=jdbc:mysql://<your-endpoint>:3306/<your-database-name>
-username=<your-username>
-password=<your-password>
+db_url=jdbc:mysql://<your-endpoint>:3306/<your-database-name>
+db_username=<your-username>
+db_password=<your-password>
 ```
 
 Adapt URL according to what you're testing (RDS, or basic mysql).
 
 Then, run either `App` or `JdbcBoilerplate` depending on what you want to test using your IDE.
+
+## Docker
+
+Fill the `.env` file using `.env.example` as a template.
+
+Build the project using the following command:
+
+```bash
+./mvnw clean package
+```
+
+Then, run the following command:
+
+```bash
+docker-compose build && docker-compose up
+```
--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -17,15 +17,28 @@ public class Config {
     private String password;
 
     private Config() {
-        Properties properties = new Properties();
-        try (InputStream inputStream = new FileInputStream("src/main/resources/application.properties")) {
-            properties.load(inputStream);
-            this.url = properties.getProperty("url");
-            this.username = properties.getProperty("username");
-            this.password = properties.getProperty("password");
+        this.url = getProperty("db_url");
+        this.username = getProperty("db_username");
+        this.password = getProperty("db_password");
+    }
+
+    private String getProperty(String key) {
+        String value;
+        try {
+            Properties properties = new Properties();
+            try (InputStream inputStream = new FileInputStream("src/main/resources/application.properties")) {
+                properties.load(inputStream);
+                value = properties.getProperty(key);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            value = System.getenv(key);
         }
+
+        if (value == null) {
+            throw new IllegalArgumentException("Property '" + key + "' not found");
+        }
+
+        return value;
     }
 
     public static Config getInstance() {
@@ -39,4 +52,3 @@ public class Config {
         return instance;
     }
 }
-
